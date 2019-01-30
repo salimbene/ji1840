c303f20c404e50fd88bb60ac03c50c69a8247f3b
Matias Salimbene <msalimbe@ar.ibm.com>
2019-01-30 00:28:26 -0300

    listgroup component

--- client/src/components/Units.jsx
+++ client/src/components/Units.jsx
@@ -6,20 +6,24 @@ import {
   addUnit
 } from '../services/unitsService';
 import Pagination from './common/pagination';
+import ListGroup from './common/listGroup';
 import { paginate } from '../utils/paginate';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { debuglog } from 'util';
 
 class Units extends Component {
   state = {
     units: {},
     pageSize: 1,
-    currentPage: 1
+    currentPage: 1,
+    floor: [{ name: 'a', id: 0 }, { name: 'b', id: 1 }, { name: 'c', id: 2 }]
   };
 
   async componentDidMount() {
+    const floor = [{ name: 'all' }, ...this.state.floor];
     const { data: units } = await getUnits();
-    this.setState({ units });
+    this.setState({ units, floor });
   }
 
   renderTable = unit => {
@@ -89,58 +93,81 @@ class Units extends Component {
     this.setState({ currentPage: page });
   };
 
+  handleFloorSelect = item => {
+    this.setState({ selectedFloor: item, currentPage: 1 });
+  };
+
   render() {
     const { length: count } = this.state.units;
-    const { units: allUnits, pageSize, currentPage } = this.state;
+    const {
+      units: allUnits,
+      pageSize,
+      currentPage,
+      selectedFloor
+    } = this.state;
 
     if (count === 0) return <p>No existen unidades funcionales registradas.</p>;
 
-    const units = paginate(allUnits, currentPage, pageSize);
+    const filtered =
+      selectedFloor && selectedFloor.id
+        ? allUnits.filter(u => u.floor === selectedFloor.id)
+        : allUnits;
+
+    const units = paginate(filtered, currentPage, pageSize);
 
     return (
-      <React.Fragment>
-        <ToastContainer />
-        <button
-          onClick={event => this.handleAddUnit(event)}
-          className="btn btn-info btn-sm"
-        >
-          Add
-        </button>
-        <button
-          onClick={event => this.handleUpdate(this.state.units[1])}
-          className="btn btn-info btn-sm"
-        >
-          Update
-        </button>
-        <button
-          onClick={event => this.handleDelete(this.state.units[1])}
-          className="btn btn-danger btn-sm"
-        >
-          Del
-        </button>
-        {this.state.units.length > 0 ? (
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">Propietario</th>
-                <th scope="col">Unidad Funcional</th>
-                <th scope="col">Piso</th>
-                <th scope="col">Puerta</th>
-                <th scope="col">Participación</th>
-              </tr>
-            </thead>
-            <tbody>{this.renderTable(units)}</tbody>
-          </table>
-        ) : (
-          <p>No hay mas nada!</p>
-        )}
-        <Pagination
-          itemsCount={count}
-          pageSize={pageSize}
-          currentPage={currentPage}
-          onPageChange={this.handlePageChange}
-        />
-      </React.Fragment>
+      <div className="row">
+        <div className="col-2">
+          <ListGroup
+            items={this.state.floor}
+            onItemSelect={this.handleFloorSelect}
+            selectedItem={this.state.selectedFloor}
+          />
+        </div>
+        <div className="col">
+          <ToastContainer />
+          <button
+            onClick={event => this.handleAddUnit(event)}
+            className="btn btn-info btn-sm"
+          >
+            Add
+          </button>
+          <button
+            onClick={event => this.handleUpdate(this.state.units[1])}
+            className="btn btn-info btn-sm"
+          >
+            Update
+          </button>
+          <button
+            onClick={event => this.handleDelete(this.state.units[1])}
+            className="btn btn-danger btn-sm"
+          >
+            Del
+          </button>
+          {this.state.units.length > 0 ? (
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">Propietario</th>
+                  <th scope="col">Unidad Funcional</th>
+                  <th scope="col">Piso</th>
+                  <th scope="col">Puerta</th>
+                  <th scope="col">Participación</th>
+                </tr>
+              </thead>
+              <tbody>{this.renderTable(units)}</tbody>
+            </table>
+          ) : (
+            <p>No hay mas nada!</p>
+          )}
+          <Pagination
+            itemsCount={filtered.length}
+            pageSize={pageSize}
+            currentPage={currentPage}
+            onPageChange={this.handlePageChange}
+          />
+        </div>
+      </div>
     );
   }
 }
---
+++ client/src/components/common/listGroup.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ListGroup = props => {
+  const {
+    items,
+    textProperty,
+    valueProperty,
+    onItemSelect,
+    selectedItem
+  } = props;
+
+  return (
+    <ul className="list-group">
+      {items.map(item => (
+        <li
+          onClick={() => onItemSelect(item)}
+          key={item[valueProperty]}
+          className={
+            item === selectedItem ? 'list-group-item active' : 'list-group-item'
+          }
+        >
+          {item[textProperty]}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+ListGroup.defaultProps = {
+  textProperty: 'name',
+  valueProperty: 'id'
+};
+
+ListGroup.propTypes = {
+  pisos: PropTypes.array.isRequired,
+  onItemSelect: PropTypes.func.isRequired
+};
+
+export default ListGroup;
