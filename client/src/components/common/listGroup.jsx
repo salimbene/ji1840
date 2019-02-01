ff7fc621383767ace519ce8472ada4dd7c1d3e02
Matias Salimbene <msalimbe@ar.ibm.com>
2019-02-01 14:22:17 -0300

    Adding sorting to table

--- client/src/components/common/listGroup.jsx
+++ client/src/components/common/listGroup.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ListGroup = props => {
-  const {
-    items,
-    textProperty,
-    valueProperty,
-    onItemSelect,
-    selectedItem
-  } = props;
-
+const ListGroup = ({
+  items,
+  textProperty,
+  valueProperty,
+  onItemSelect,
+  selectedItem
+}) => {
   return (
     <ul className="list-group">
       {items.map(item => (
