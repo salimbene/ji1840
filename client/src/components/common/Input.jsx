e140f17c2552fb9847f5dcaea51bb59a8d6e949d
Matias Salimbene <msalimbe@ar.ibm.com>
2019-02-04 00:10:57 -0300

    cambio estilo login

--- client/src/components/common/Input.jsx
+++ client/src/components/common/Input.jsx
@@ -6,10 +6,19 @@ const Input = ({ name, label, error, ...rest }) => {
       <label className="label" htmlFor={name}>
         {label}
       </label>
-      <input {...rest} name={name} id={name} className="form-control" />
-      {error && (
+      <input
+        {...rest}
+        name={name}
+        id={name}
+        className={`
+          form-control 
+          ${error && 'is-invalid'}
+        `}
+      />
+      {error && <div className="invalid-feedback">{error}</div>}
+      {/* {error && (
         <div className="alert alert-danger col-sm-10 opacity">{error}</div>
-      )}
+      )} */}
     </div>
   );
 };
