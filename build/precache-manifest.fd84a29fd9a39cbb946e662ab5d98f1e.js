5403379b26d3b9e5c05acdc9c0e62110eb02936f
Matias Salimbene <msalimbe@ar.ibm.com>
2019-04-20 12:54:26 -0300

    new build

---
+++ build/precache-manifest.fd84a29fd9a39cbb946e662ab5d98f1e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "fcbbce479f5ee7bdee6e",
+    "url": "/static/css/main.6db2c08a.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fbce895a1434acc47c0e1bf0b30e8b27",
+    "url": "/static/media/ji1840.fbce895a.jpg"
+  },
+  {
+    "revision": "f767e49b01148b27db04",
+    "url": "/static/js/2.83741203.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fcbbce479f5ee7bdee6e",
+    "url": "/static/js/main.1ceed1f9.chunk.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "f767e49b01148b27db04",
+    "url": "/static/css/2.f6a71665.chunk.css"
+  },
+  {
+    "revision": "e71c73eafa6c11dd3bc10440c047e63d",
+    "url": "/index.html"
+  }
+];
